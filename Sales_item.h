0a7b3a79562a59e3af9b0abb4977784d19257629
zhangsaiping <1145178427@qq.com>
2016-04-15 09:59:49 +0800

    a little modified

--- Sales_item.h
+++ Sales_item.h
@@ -1,14 +1,16 @@
-#include ＜string＞
+/*#include ＜string＞
 class Sales_item{
 public:
 	std::string ISBN;
-	int NumbersofSale;
-	double price;
+	int NumbersofSale=0;//进行赋值初始化
+	double price=0.0;
 	//一般使用double而不使用float，因为float会有精度损失，而double引入双精度却没有带来什么性能损失
-};
+};*/
 //一般类的定义放在头文件里
+
 //private:只能被该类的函数访问
 //protected:可以被该类的函数和子类的函数访问
 //public:可以被该类的函数和子类的函数和该类的对象访问
+
 //class默认成员都是private
 //struct默认成员都是public
--- test1.h
+++ test1.h
@@ -1,4 +1,4 @@
 //extern int age;//只声明不定义
 
-//const变量如果用常量表达式初始化，那么可以将其放在头文件中定义初始化，要使用const变量的文件只需要包含该头文件即可
-//如果const变量不是用常量表达式初始化，那么需要在源文件中定义初始化，加关键字extern，要使用const变量的文件只需要写extern const...声明即可，保证其他文件可使用该变量
+//对于const依旧可以
+//extern const int buffersize;
